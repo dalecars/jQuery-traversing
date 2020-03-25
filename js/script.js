33222974f323c2d95cb14e9453dd5467c11b0644
dalecars <carson.dale@gmail.com>
2020-03-25 00:40:58 +0000

    inital commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+//wait until page is ready
+$(document).ready(function() {
+	//sets <a> element within paragraph  to yellow  
+	$("p").click(function(){
+		$(this).children("a").css("background-color", "yellow"); /* returns all the <a> child elements that are 
+		within this paragraph*/
+	});
+
+	// Toggle the visibility of the paragraph when a button is clicked 
+	$("button").click(function(){
+		$(this).prev().slideToggle('slow');
+	});
+
+	// Open the paragraph once the image is clicked
+	$("img").click(function() {
+		$(this).next().children("p").slideDown();
+	});
+
+    // highlight the card once the image is clicked and unhighlight when clicked again
+    $(".card").click(function() {
+    $(this).toggleClass("highlight_card");
+ 
+    // All cards that are not currently selected will be hidden when `select_btn` is clicked
+    $("#select_btn").click(function() {
+		$(".card:not(.highlight_card)").hide();	 
+    });
+    
+    // Select all cards
+	$("#all_btn").click(function(){
+		$(".card").show();	 
+	});
+});
+});
